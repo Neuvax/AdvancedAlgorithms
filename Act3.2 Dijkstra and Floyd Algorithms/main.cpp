e910120f09de848da92500ef7c7e74eb8ea5f6b9
Armando Terrazas Gómez <A01640924@tec.mx>
2023-09-25 10:40:05 -0600

    Set up

---
+++ Act3.2 Dijkstra and Floyd Algorithms/main.cpp
@@ -0,0 +1,33 @@
+//#pragma GCC optimize("Ofast", "unroll-loops", "no-stack-protector", "fast-math")
+//#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli, lli> ii;
+typedef vector<lli> vi;
+
+#define f first
+#define s second
+#define endl '\n'
+#define pb push_back
+#define sz(s) lli(s.size())
+#define all(s) begin(s), end(s)
+#define deb(x) cout << #x": " << (x) << endl
+#define print(x) cout << (x) << endl
+#define fore(i, a, b) for(lli i = (a), BS = (b); i < BS; ++i)
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() { _
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    lli N, E;
+    cin >> N >> E;
+    vector<vector<ii>> adj(N);
+    
+    return 0;
+}
+
+//g++-13 -std=c++20 main.cpp && ./a.out < input.txt > output.txt
