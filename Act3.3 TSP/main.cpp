0868455beaf71cb6e2ce99af6453d2f5bdedf293
Armando Terrazas Gómez <A01640924@tec.mx>
2023-10-05 17:20:41 -0600

    Activity 3.3 Nearest Neighbor

---
+++ Act3.3 TSP/main.cpp
@@ -0,0 +1,120 @@
+/**
+ * @file main.cpp
+ * @author Jorge Germán Wolburg Trujillo -- A01640826
+ * @author Armando Terrazas Gómez -- A01640924 
+ * @brief The Salesman Problem solution using Nearest Neighbor
+ * @version 0.1
+ * @date 05-10-2023
+*/
+//#pragma GCC optimize("Ofast", "unroll-loops", "no-stack-protector", "fast-math")
+//#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long double ld;
+typedef long long lli;
+typedef pair<lli, lli> ii;
+typedef vector<lli> vi;
+
+#define f first
+#define s second
+#define endl '\n'
+#define pb push_back
+#define sz(s) lli(s.size())
+#define all(s) begin(s), end(s)
+#define deb(x) cout << #x": " << (x) << endl
+#define print(x) cout << (x) << endl
+#define fore(i, a, b) for(lli i = (a), BS = (b); i < BS; ++i)
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+/**
+ * @brief City class
+ * 
+ */
+class City {
+    private:
+        lli id;
+        lli x;
+        lli y;
+    public:
+        /**
+         * @brief Construct a new City object
+         * 
+         * @param id 
+         * @param x 
+         * @param y 
+         */
+        City(lli id = -1, lli x = -1, lli y = -1) {
+            this->id = id;
+            this->x = x;
+            this->y = y;
+        }
+
+        /**
+         * @brief Get the Distance To object
+         * 
+         * @param c City to calculate distance
+         * @return lli square distance between cities 
+         */
+        lli getDistanceTo(const City &c) {
+            //Calculate square distance between cities
+            lli d1 = (this->x - c.x) * (this->x - c.x) + (this->y - c.y) * (this->y - c.y);
+            return d1;
+        }
+};
+
+/**
+ * @brief Nearest Neighbor algorithm
+ * 
+ * @param currentCity to calculate nearest neighbor  
+ * @param cities vector of cities 
+ * @param visitedCities vector of visited cities 
+ * @return pair<lli, lli> nearest neighbor and distance 
+ */
+ii nearestNeighbor(City currentCity, vector<City> &cities, vector<bool> &visitedCities) {
+    lli minDistance = numeric_limits<lli>::max();
+    lli nearestCity = -1;
+    fore(i, 0, sz(cities)) {
+        if (visitedCities[i]) continue;
+        lli distance = currentCity.getDistanceTo(cities[i]);
+        if (distance < minDistance) {
+            minDistance = distance;
+            nearestCity = i;
+        }
+    }
+    return make_pair(nearestCity, minDistance);
+}
+
+int main() { _
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    lli n;
+    cin >> n;
+    vector<City> cities(n);
+    fore(i, 0, n) {
+        lli x, y;
+        cin >> x >> y;
+        cities[i] = City(i, x, y);
+    }
+
+    vector<bool> visitedCities(n, false);
+    lli totalDistance = 0;
+    lli currentCity = 0;
+    visitedCities[currentCity] = true;
+    fore(i, 0, n - 1) {
+        ii nearestCity = nearestNeighbor(cities[currentCity], cities, visitedCities);
+        currentCity = nearestCity.f;
+        totalDistance += nearestCity.s;
+        visitedCities[currentCity] = true;
+    }
+
+    totalDistance += cities[currentCity].getDistanceTo(cities[0]);
+    //Calculate square root of total distance
+    ld ans = sqrt(totalDistance);
+    cout << fixed << setprecision(2) << ans << endl;
+
+    return 0;
+}
+
+//g++-13 -std=c++20 main.cpp && ./a.out < input.txt > output.txt
