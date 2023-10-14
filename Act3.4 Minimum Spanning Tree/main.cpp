93f623e2ef8545db40f78b742956c4410338bd23
Neuvax <a01640826@tec.mx>
2023-10-14 17:01:00 -0600

    MST Added

---
+++ Act3.4 Minimum Spanning Tree/main.cpp
@@ -0,0 +1,85 @@
+//#pragma GCC optimize("Ofast", "unroll-loops", "no-stack-protector", "fast-math")
+//#pragma GCC target("avx,avx2,fma")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long lli;
+typedef pair<lli, pair<lli, lli>> edge;
+
+#define f first
+#define s second
+#define pb push_back
+#define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+class DSU {
+private:
+    vector<lli> parent, rank;
+public:
+    DSU(lli n) {
+        rank.resize(n, 0);
+        parent.resize(n);
+        for(lli i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+    }
+
+    lli find(lli x) {
+        if(parent[x] != x) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+
+    void unionSet(lli x, lli y) {
+        lli rootX = find(x);
+        lli rootY = find(y);
+
+        if(rootX != rootY) {
+            if(rank[rootX] < rank[rootY]) {
+                parent[rootX] = rootY;
+            } else if(rank[rootX] > rank[rootY]) {
+                parent[rootY] = rootX;
+            } else {
+                parent[rootY] = rootX;
+                rank[rootX]++;
+            }
+        }
+    }
+};
+
+int main() { _
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+
+    lli n, m, a, b, w, mstWeight = 0;
+    cin >> n >> m;
+
+    vector<edge> edges;
+    lli minVertex = INT_MAX;  // to detect the starting vertex (either 0 or 1)
+    for(lli i = 0; i < m; i++) {
+        cin >> a >> b >> w;
+        edges.pb({w, {a, b}});
+        minVertex = min(minVertex, min(a, b));
+    }
+
+    sort(edges.begin(), edges.end());
+
+    DSU dsu(n + 1);
+
+   for(auto e : edges) {
+    lli u = e.s.f, v = e.s.s;
+    if(minVertex == 1) {
+        u -= 1;
+        v -= 1;
+    }
+    if(dsu.find(u) != dsu.find(v)) {
+        mstWeight += e.f;
+        dsu.unionSet(u, v);
+    }
+}
+
+    cout << mstWeight << endl;
+
+    return 0;
+}
