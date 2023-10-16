445b5ba4dd8d7abb783c5ef7d989d1fca4a83a06
Neuvax <a01640826@tec.mx>
2023-10-16 10:22:03 -0600

    Complexity Added

--- Act3.4 Minimum Spanning Tree/main.cpp
+++ Act3.4 Minimum Spanning Tree/main.cpp
@@ -5,9 +5,8 @@
  * @brief Minimum Spanning Tree (MST) using Kruskal's algorithm
  * @version 0.1
  * @date 14-10-2023
-*/
-//#pragma GCC optimize("Ofast", "unroll-loops", "no-stack-protector", "fast-math")
-//#pragma GCC target("avx,avx2,fma")
+ */
+
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -27,15 +26,27 @@ typedef pair<lli, pair<lli, lli>> edge;
 class DSU {
 private:
     vector<lli> parent, rank;
+
 public:
+    /**
+     * @brief Constructor to initialize DSU data structure.
+     * @param n Total number of elements/nodes.
+     */
     DSU(lli n) {
-        rank.resize(n, 0);
-        parent.resize(n);
+        rank.resize(n, 0); 
+        parent.resize(n); 
         for(lli i = 0; i < n; ++i) {
             parent[i] = i;
         }
     }
 
+    /**
+     * @brief Find the representative of the set containing the element.
+     * @param x The node to find the set representative for.
+     * @return The set representative of node x.
+     * 
+     * Complexity: O(1) due to path compression
+     */
     lli find(lli x) {
         if(parent[x] != x) {
             parent[x] = find(parent[x]);
@@ -47,6 +58,8 @@ public:
      * @brief Merges two sets together.
      * @param x First node.
      * @param y Second node.
+     * 
+     * Complexity: O(1) due to union by rank
      */
     void unionSet(lli x, lli y) {
         lli rootX = find(x);
@@ -66,37 +79,36 @@ public:
 };
 
 int main() { _
-    //freopen("input.txt", "r", stdin);
-    //freopen("output.txt", "w", stdout);
-
     lli n, m, a, b, w, mstWeight = 0;
     cin >> n >> m;
 
-    vector<edge> edges;
+    vector<edge> edges;   
     lli minVertex = INT_MAX;  // to detect the starting vertex (either 0 or 1)
-    for(lli i = 0; i < m; i++) {
+    for(lli i = 0; i < m; i++) {  // O(m)
         cin >> a >> b >> w;
         edges.pb({w, {a, b}});
         minVertex = min(minVertex, min(a, b));
     }
 
-    sort(edges.begin(), edges.end());
+    sort(edges.begin(), edges.end());  // O(m log m)
 
-    DSU dsu(n + 1);
+    DSU dsu(n + 1);  // O(n)
 
-   for(auto e : edges) {
-    lli u = e.s.f, v = e.s.s;
-    if(minVertex == 1) {
-        u -= 1;
-        v -= 1;
+    for(auto e : edges) {  // O(m)
+        lli u = e.s.f, v = e.s.s;
+        if(minVertex == 1) {
+            u -= 1;
+            v -= 1;
+        }
+        if(dsu.find(u) != dsu.find(v)) {  // O(1)
+            mstWeight += e.f;
+            dsu.unionSet(u, v);  // O(1)
+        }
     }
-    if(dsu.find(u) != dsu.find(v)) {
-        mstWeight += e.f;
-        dsu.unionSet(u, v);
-    }
-}
 
     cout << mstWeight << endl;
 
     return 0;
 }
+
+// Overall Complexity for the MST: O(m log m)
