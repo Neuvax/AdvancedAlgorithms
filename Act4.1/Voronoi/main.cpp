f7dc3a78ee7162561653cd417969cbb0ff05fd63
Armando Terrazas Gómez <A01640924@tec.mx>
2023-11-07 07:45:22 -0600

    Voronoi set up

---
+++ Act4.1/Voronoi/main.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <string> // This must be defined before including CGAL draw headers
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_2<K> Delaunay;
+typedef K::Point_2 Point;
+
+struct Toilet {
+    std::string line;
+    std::string station;
+    std::string accessible;
+    std::string tariff;
+    std::string passNavigoAccess;
+    std::string pushButtonAccess;
+    std::string inControlledArea;
+    std::string outControlledAreaStation;
+    std::string outControlledAreaPublicWay;
+    std::string pmrAccessibility; // Accessibilite PMR
+    std::string location; // Localisation
+    double latitude;
+    double longitude;
+    std::string manager; // Gestionnaire
+};
+
+std::vector<Toilet> read_csv(const std::string& filename) {
+    std::vector<Toilet> toilets;
+    std::ifstream file(filename);
+    std::string line;
+    // Skip the header
+    std::getline(file, line);
+
+    while (std::getline(file, line)) {
+        std::istringstream s(line);
+        std::string field;
+        Toilet toilet;
+
+        std::getline(s, toilet.line, ';');
+        std::getline(s, toilet.station, ';');
+        std::getline(s, toilet.accessible, ';');
+        std::getline(s, toilet.tariff, ';');
+        std::getline(s, toilet.passNavigoAccess, ';');
+        std::getline(s, toilet.pushButtonAccess, ';');
+        std::getline(s, toilet.inControlledArea, ';');
+        std::getline(s, toilet.outControlledAreaStation, ';');
+        std::getline(s, toilet.outControlledAreaPublicWay, ';');
+        std::getline(s, toilet.pmrAccessibility, ';');
+        std::getline(s, toilet.location, ';');
+
+        // Parse coordinates
+        std::getline(s, field, ';');
+        std::istringstream coord_stream(field);
+        std::string lat, lng;
+        std::getline(coord_stream, lat, ',');
+        std::getline(coord_stream, lng, ',');
+        toilet.latitude = std::stod(lat);
+        toilet.longitude = std::stod(lng);
+
+        std::getline(s, toilet.manager, ';');
+
+        toilets.push_back(toilet);
+    }
+    return toilets;
+}
+
+int main(int argc, char* argv[]) {
+    std::string csv_file = "toilets.csv";
+    std::vector<Toilet> toilets = read_csv(csv_file);
+    
+    // Construct Voronoi Diagram
+    std::vector<Point> points;
+    for (const auto& toilet : toilets) {
+        points.push_back(Point(toilet.longitude, toilet.latitude));
+    }
+
+    Delaunay dt;
+    dt.insert(points.begin(), points.end());
+
+    // Querying the nearest toilet example (for illustration)
+    // Here you would get the query from user input or another source
+    Point query(2.3522 /* longitude of Paris */, 48.8566 /* latitude of Paris */);
+    auto nearest = dt.nearest_vertex(query);
+    std::cout << "The nearest toilet is at: "
+              << nearest->point().x() << ", " << nearest->point().y() << std::endl;
+
+    return 0;
+}
